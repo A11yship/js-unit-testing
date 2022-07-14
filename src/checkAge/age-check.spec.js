99329dc428e78562c02f1f0251d7298c20283cfb
Hannah Steil <h-steil@t-online.de>
2022-07-14 13:05:54 +0200

    Add tests with age under 16

--- src/checkAge/age-check.spec.js
+++ src/checkAge/age-check.spec.js
@@ -5,4 +5,16 @@ describe("ageCheck", () => {
     const result = ageCheck();
     expect(result).toBe("Error: no age");
   });
+  it('returns a String "Too young" if called ageCheck(0)', () => {
+    const result = ageCheck(0);
+    expect(result).toBe("Too young");
+  });
+  it('returns a String "Too young" if called ageCheck(9)', () => {
+    const result = ageCheck(9);
+    expect(result).toBe("Too young");
+  });
+  it('returns a String "Too young" if called ageCheck(15)', () => {
+    const result = ageCheck(15);
+    expect(result).toBe("Too young");
+  });
 });
